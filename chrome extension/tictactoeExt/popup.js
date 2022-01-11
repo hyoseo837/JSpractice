505206a99b57fb89da5d13e2a0f7489043fe6713
Ugly_Potato <71345965+hyoseo837@users.noreply.github.com>
2022-01-11 23:31:47 +0100

    board complete

--- chrome extension/tictactoeExt/popup.js
+++ chrome extension/tictactoeExt/popup.js
@@ -5,15 +5,37 @@ let colors = ["tomato","skyblue"]
 
 let para = document.getElementById("demo")
 let btns = []
+btns.push(document.getElementById('0'))
+btns.push(document.getElementById('1'))
+btns.push(document.getElementById('2'))
+btns.push(document.getElementById('3'))
+btns.push(document.getElementById('4'))
+btns.push(document.getElementById('5'))
+btns.push(document.getElementById('6'))
+btns.push(document.getElementById('7'))
+btns.push(document.getElementById('8'))
+let game = document.getElementById("game")
+
+
 for(let i=0; i<9;i++){
-    btns.push(document.getElementById(toString(i)))
-}
-console.log(btns)
-for(let i=0; i<9;i++){
-    btns[i].addEventListener("click",(turn)=>{
+    btns[i].addEventListener("click",()=>{
         if (board[i] == ""){
-            board[i] = turn;
+            board[i] = sign[turn];
+            turn = Number(!turn);
             btns[i].style.backgroundColor = colors[turn];
         }
     })
+}
+
+game.addEventListener("click",()=>{
+    board = ["","","","","","","","",""];
+    turn = 0;
+    for(let i=0; i<9;i++){
+        btns[i].style.backgroundColor = "#cdcdcd"
+    }
+
+})
+
+function checkwin(){
+    
 }
